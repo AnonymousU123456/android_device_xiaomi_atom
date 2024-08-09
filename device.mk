36f9d6eee38d3077889f512cfc71dab62b7a9b95
xjl12 <xiao-xjle@qq.com>
2024-08-09 20:22:04 +0800

    refactor(IMS): Reorganize the directory structure of IMS prebuilt jars

--- device.mk
+++ device.mk
@@ -245,33 +245,13 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-	ImsService \
-	EngineerMode \
 	libem_support_jni \
     libem_aoltest_jni \
     libem_audio_jni \
     libem_wifi_jni \
     libem_bleoffload_jni \
-    mediatek-carrier-config-manager \
-	mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-ims-extension-plugin \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
 
-	
-PRODUCT_BOOT_JARS += \
-    mediatek-carrier-config-manager \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
+$(call inherit-product, $(LOCAL_PATH)/prebuilt/ims-prebuilt.mk)
 
 # INIT
 
